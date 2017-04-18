9f617bc88762e68872a5cb6e05a5d6e12bcf5a3b
Tu Ngo <tungo8810@gmail.com>
2017-04-18 11:51:38 -0700

    Add edit form; abstract new and edit forms to _form

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -13,6 +13,34 @@ class CatsController < ApplicationController
   end
 
   def new
-    
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :cats, only: [:index, :show, :new, :create]
+  resources :cats, only: [:index, :show, :new, :create, :edit, :update]
 end
