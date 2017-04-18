ab69fec13f27b21658a73fa300ad299ab8904a67
Tu Ngo <tungo8810@gmail.com>
2017-04-18 15:27:04 -0700

    Refactor cats/show.html

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :cats, only: [:index, :show, :new, :create, :edit, :update]
+  resources :cats, except: :destroy
 
   resources :cat_rental_requests, only: [:new, :create]
 
