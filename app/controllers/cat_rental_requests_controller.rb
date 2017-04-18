d86e14e03c3685d402a6c7f94dbd0dbbc3393d91
Tu Ngo <tungo8810@gmail.com>
2017-04-18 14:14:49 -0700

    Add CatRentalRequests Controller functionality

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -1,6 +1,23 @@
 class CatRentalRequestsController < ApplicationController
 
   def new
-    
+
+  end
+
+  def create
+    @rental = CatRentalRequest.new(rental_params)
+    @rental.status = 'PENDING'
+    if @rental.save
+      redirect_to cat_url(@rental.cat_id)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:cat_id, :start_date, :end_date)
   end
 end
